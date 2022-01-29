1b45cf5f6ed93167e0728faa6d6abb1aae68754d
DavidvK17 <david.sing7@gmail.com>
2022-01-29 17:03:21 +0100

    Implement static Login for first user

--- REAMDE.md
+++ REAMDE.md
@@ -4,12 +4,12 @@ This is my third project, mimicking a Single Page Application (SPA) build in Van
 
 There exist 4 individual user accounts (here represented by objects, normally we receive this Data through an API). Each account has individual movements, an individual interest rate and the user has their own pin.
 
+- create userName
 - implement login (to be implemented)
-
-* display movements (to be implemented)
-* calculate balance (to be implemented)
-* calculate interest (to be implemented)
-* transfer money (to be implemented)
-* request loan (to be implemented)
-* close account (to be implemented)
-* implement timer on login (to be implemented)
+- display movements (to be implemented)
+- calculate balance (to be implemented)
+- calculate interest (to be implemented)
+- transfer money (to be implemented)
+- request loan (to be implemented)
+- close account (to be implemented)
+- implement timer on login (to be implemented)
--- script.js
+++ script.js
@@ -1,1 +1,61 @@
 'use strict';
+
+// Data
+const account1 = {
+  owner: 'David Singer',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111
+};
+
+const account2 = {
+  owner: 'Marek Singer',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222
+};
+
+const account3 = {
+  owner: 'Krystyna Singer',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333
+};
+
+const account4 = {
+  owner: 'Arad von Kiefersfelden',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// create userName
+accounts.map((acc, i) => {
+  const firstInitial = acc.owner.toLowerCase().split(' ')[0][0];
+  const secondInitial = acc.owner.toLowerCase().split(' ')[1][0];
+  acc.userName = firstInitial.concat(secondInitial);
+  console.log(acc);
+});
+
+// Login
+const loginBtn = document.getElementById('login--btn');
+const user = document.getElementById('login--user');
+const userPIN = document.getElementById('login--pin');
+
+const movements = document.getElementsByClassName('movements')[0];
+console.log(movements);
+
+loginBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  console.log(user.value, userPIN.value);
+  if (
+    user.value === account1.userName &&
+    Number(userPIN.value) === account1.pin
+  ) {
+    movements.classList.add('active');
+  } else {
+    console.log('wrong input');
+  }
+});
